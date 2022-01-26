c592900d2ef79e5bcb0d09477e9b7b4f772964c9
HelenCris <79730539+HelenCris@users.noreply.github.com>
2022-01-26 10:42:22 -0300

    atualizado o caminho pro post

--- calculator.js
+++ calculator.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/index.html", function(req, res){
 
     var num1 = Number(req.body.n1);
     var num2 = Number(req.body.n2);
@@ -18,7 +18,6 @@ app.post("/", function(req, res){
     res.send("The result of the calculation is " + result);
 });
 
-  
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
